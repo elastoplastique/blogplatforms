2b5f082778168f226fb540460aedd3f5301a8fff
Elastoplastique <elastoplastique@gmail.com>
2023-11-09 03:51:20 +0300

    swiper CSS files commented.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import '@/styles/globals.css';
-import 'swiper/css';
-import 'swiper/css/a11y';
-import 'swiper/css/autoplay';
-import 'swiper/css/keyboard';
-import 'swiper/css/pagination';
-import 'swiper/css/parallax';
+// import 'swiper/css';
+// import 'swiper/css/a11y';
+// import 'swiper/css/autoplay';
+// import 'swiper/css/keyboard';
+// import 'swiper/css/pagination';
+// import 'swiper/css/parallax';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@/components/theme-provider';
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
