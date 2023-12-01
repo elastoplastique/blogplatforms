d4776443f97028567922d346a29e95b73e783301
Elastoplastique <elastoplastique@gmail.com>
2023-12-01 15:24:09 +0300

    footer

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -36,3 +36,85 @@ export const FOOTER_CONTENT = {
     text: '',
   },
 };
+
+
+export const FOOTER_FEATURE_LINKS: NavLinkType[] = [
+  {
+    path: '/features/headless',
+    text: 'Headless',
+    description: 'Headless blogging platforms',
+  },
+  {
+    path: '/features/ad-monetization',
+    text: 'Ad Monetization',
+    description: 'Blogging platforms supporting ad monetization',
+  },
+  {
+    path: '/features/category-tag-system',
+    text: 'Category & Tag System',
+    description: 'Blog platforms supporting category & tag system',
+  },
+  {
+    path: '/features/comment-system',
+    text: 'Comment System',
+    description: 'Blog platforms supporting commenting',
+  },
+  {
+    path: '/features/figma',
+    text: 'Figma Integration',
+    description: 'Blog platforms supporting Figma integration',
+  },
+  {
+    path: '/features/image-optimization',
+    text: 'Advanced Image Optimization',
+    description: 'Blog platforms supporting advanced image optimization',
+  },
+  {
+    path: '/features/integrated-reader-base',
+    text: 'Integrated Reader Base',
+    description: 'Blog platforms have integrated reader base',
+  },
+  {
+    path: '/features/open-source',
+    text: 'Open-source',
+    description: 'Open-source blogging platforms',
+  },
+  {
+    path: '/features/sell-digital-products',
+    text: 'Sell Digital Products',
+    description: 'Blog platforms supporting selling digital products',
+  },
+];
+
+export const FOOTER_BLOG_LINKS: NavLinkType[] = [
+  {
+    path: '/blog/create-a-blog-site',
+    text: 'How to create a blog site',
+    description: 'How to create a blog site',
+  },
+  {
+    path: '/blog/best-portfolio-sites',
+    text: 'Best Portfolio Sites',
+    description: 'Best portfolio sites',
+  },
+  {
+    path: '/blog/best-free-platforms-for-blogging',
+    text: 'Best Free Platforms for Blogging',
+    description: 'A list of the best free platforms for blogging',
+  },
+  {
+    path: '/blog/best-blogging-platforms-for-developers',
+    text: 'Best Blogging Platforms for Developers',
+    description: 'A list of the best blogging platforms for developers',
+  },
+  {
+    path: '/blog/best-blog-platforms-for-artists',
+    text: 'Best Blogging Platforms for Artists',
+    description: 'A list of the best blogging platforms for artists',
+  },
+  {
+    path: '/blog/best-blog-platforms-for-designers',
+    text: 'Best Blogging Platforms for Designers',
+    description: 'A list of the best blogging platforms for designers',
+  },
+];
---
+++ src/constants/social-media.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_MEDIA = {
+    TWITTER: 'https://twitter.com/bloggingapp',
+    PINTEREST: 'https://www.pinterest.com/bloggingplatforms/',
+    GITHUB: 'https://github.com/elastoplastique'
+}
