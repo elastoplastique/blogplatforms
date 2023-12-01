00ad55e36bdbad6e26d4cc8604a86aba35304b3e
Elastoplastique <elastoplastique@gmail.com>
2023-12-02 01:27:11 +0300

    meta description has changed

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -4,11 +4,11 @@ export const META = {
   TITLE: 'Best Blogging Platforms & Sites | BloggingPlatforms.app',
   IMAGE: 'https://bloggingplatforms.app/assets/home-light.jpg',
   DESCRIPTION:
-    'Discover and compare the best blogging sites and platforms at BloggingSites.app. Our curated list provides in-depth reviews, helping you make an informed decision for your blogging journey.',
+    'Discover and compare the best blogging sites and platforms at Blogging Platforms. Our curated list provides in-depth reviews, helping you make an informed decision for your blogging journey.',
   HOME: {
     TITLE: 'Best Blogging Platforms',
     DESCRIPTION:
-      'Discover and compare the best blogging sites and platforms at BloggingSites.app. Our curated list provides in-depth reviews, helping you make an informed decision for your blogging journey.',
+      'Discover and compare the best blogging sites and platforms at Blogging Platforms. Our curated list provides in-depth reviews, helping you make an informed decision for your blogging journey.',
     HTML_DESCRIPTION:
       '<p>Discover and compare <em>the best blogging platforms</em> and sites. <br/> Our curated list provides in-depth reviews, helping you make an informed decision for your blogging journey.</p><p><strong>Search blog platforms by features</strong></p>',
   },
