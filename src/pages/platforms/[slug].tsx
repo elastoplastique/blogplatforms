9ea87eae841d3774d0dc6cc905763519384104e5
Elastoplastique <elastoplastique@gmail.com>
2024-03-13 20:45:29 +0300

    style fix: nowrap directive for platform titles was removed

--- src/pages/platforms/[slug].tsx
+++ src/pages/platforms/[slug].tsx
@@ -123,7 +123,7 @@ export default function PlatformPage({ platform, platformFeatures, platformCompa
           </Flex>
 
           <motion.div className="relative min-w-full rounded-3xl flex flex-col justify-center items-center min-h-32 my-8">
-            <Heading as="h1" size="4" className="tracking-tight text-center !font-semi-bold !mx-8 text-inherit pt-2 whitespace-nowrap">
+            <Heading as="h1" size="4" className="tracking-tight text-center !font-semi-bold !mx-8 text-inherit pt-2 whitespace-wrap">
               <span className="text-5xl sm:text-6xl  block !tracking-tighter uppercase">{platform?.pageTitle || platform.title}</span>
             </Heading>
 
