b0773ba5735a8cde24a91a4e3df5422171153ccb
Elastoplastique <elastoplastique@gmail.com>
2024-03-14 20:43:56 +0300

    removed manifest.json

--- src/components/meta/favicons.tsx
+++ src/components/meta/favicons.tsx
@@ -7,7 +7,6 @@ export const Favicons = () => (
     <link rel="icon" type="image/png" sizes="192x192" href={`${ASSETS_DIRECTORY.FAVICON_DIRECTORY}/android-icon-192x192.png`} />
     <link rel="icon" type="image/png" sizes="32x32" href={`${ASSETS_DIRECTORY.FAVICON_DIRECTORY}/favicon-32x32.png`} />
     <link rel="icon" type="image/png" sizes="16x16" href={`${ASSETS_DIRECTORY.FAVICON_DIRECTORY}/favicon-96x96.png`} />
-    <link rel="manifest" href={`${ASSETS_DIRECTORY.FAVICON_DIRECTORY}}/manifest.json`} />
     <meta name="msapplication-TileColor" content="#ffffff" />
     <meta name="theme-color" content="#ffffff" />
   </>
