20553d3cf4197d81ffbf44d317315e7caf4e4464
Elastoplastique <elastoplastique@gmail.com>
2024-03-16 22:06:27 +0300

    free-blogging-platforms-table v2

--- public/embeds/free-blogging-platforms-table/index.js
+++ public/embeds/free-blogging-platforms-table/index.js
@@ -8,72 +8,72 @@ const rawData = [
       easeOfUse: 3,
       designFlexibility: 5,
       seoTools: 4,
-      storage: '',
-      limits: null,
+      storage: '1GB',
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
-      exportContent: { freePlan: false, paidPlan: true },
-      markdown: { freePlan: false, paidPlan: false },
+      exportContent: { freePlan: true, paidPlan: true },
+      markdown: { freePlan: true, paidPlan: true },
       openSource: { freePlan: false, paidPlan: false },
-      headless: { freePlan: false, paidPlan: false },
-      codeInject: { freePlan: false, paidPlan: true }
+      headless: { freePlan: true, paidPlan: true },
+      codeInject: { freePlan: true, paidPlan: true }
    },
    {
       name: 'WebWave',
       url: 'https://webwave.me/ref/14072231082',
-      easeOfUse: 5,
-      designFlexibility: 5,
-      seoTools: 5,
-      storage: '',
-      limits: null,
+      easeOfUse: 3,
+      designFlexibility: 4,
+      seoTools: 3,
+      storage: '1GB',
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
-      exportContent: { freePlan: false, paidPlan: true },
+      exportContent: { freePlan: false, paidPlan: false },
       markdown: { freePlan: false, paidPlan: false },
       openSource: { freePlan: false, paidPlan: false },
       headless: { freePlan: false, paidPlan: false },
-      codeInject: { freePlan: false, paidPlan: true }
+      codeInject: { freePlan: true, paidPlan: true }
    },
    {
       name: 'WordPress.com',
       url: 'https://wordpress.com/',
-      easeOfUse: 5,
-      designFlexibility: 5,
+      easeOfUse: 3,
+      designFlexibility: 4,
       seoTools: 5,
       storage: '',
-      limits: null,
+      limits: "The power of WordPress lies in plugins and themes, which are only available for paid plans.",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
-      exportContent: { freePlan: false, paidPlan: true },
-      markdown: { freePlan: false, paidPlan: false },
+      exportContent: { freePlan: true, paidPlan: true },
+      markdown: { freePlan: true, paidPlan: false },
       openSource: { freePlan: false, paidPlan: false },
-      headless: { freePlan: false, paidPlan: false },
+      headless: { freePlan: true, paidPlan: true },
       codeInject: { freePlan: false, paidPlan: true }
    },
    {
       name: 'Wix',
       url: 'https://www.wix.com',
-      easeOfUse: 5,
-      designFlexibility: 5,
-      seoTools: 5,
+      easeOfUse: 4,
+      designFlexibility: 4,
+      seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "Wix websites generally suffers from slow loading times, which negatively impacts SEO.",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
-      exportContent: { freePlan: false, paidPlan: true },
-      markdown: { freePlan: false, paidPlan: false },
+      exportContent: { freePlan: true, paidPlan: true },
+      markdown: { freePlan: true, paidPlan: true },
       openSource: { freePlan: false, paidPlan: false },
-      headless: { freePlan: false, paidPlan: false },
-      codeInject: { freePlan: false, paidPlan: true }
+      headless: { freePlan: false, paidPlan: true },
+      codeInject: { freePlan: true, paidPlan: true }
    },
    {
       name: 'Typedream',
       url: 'https://typedream.com/?via=blogplatforms',
       easeOfUse: 5,
-      designFlexibility: 5,
-      seoTools: 5,
+      designFlexibility: 3,
+      seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: false, paidPlan: true },
@@ -85,34 +85,34 @@ const rawData = [
    {
       name: 'Tilda',
       url: 'https://tilda.com/',
-      easeOfUse: 5,
-      designFlexibility: 5,
-      seoTools: 5,
+      easeOfUse: 4,
+      designFlexibility: 4,
+      seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: false, paidPlan: true },
       markdown: { freePlan: false, paidPlan: false },
       openSource: { freePlan: false, paidPlan: false },
       headless: { freePlan: false, paidPlan: false },
-      codeInject: { freePlan: false, paidPlan: true }
+      codeInject: { freePlan: true, paidPlan: true }
    },
    {
       name: 'PageCloud',
       url: 'https://www.pagecloud.com/home-partners?gspk=Y2FuYnVyYWtzb2Z5YWxpb2dsdTMwNTQ&gsxid=Is2tDreeXpFm&utm_campaign=affiliate-referral-program&utm_medium=partner&utm_source=canburaksofyalioglu3054',
-      easeOfUse: 5,
-      designFlexibility: 5,
-      seoTools: 5,
+      easeOfUse: 3,
+      designFlexibility: 2,
+      seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: false, paidPlan: true },
       markdown: { freePlan: false, paidPlan: false },
       openSource: { freePlan: false, paidPlan: false },
       headless: { freePlan: false, paidPlan: false },
-      codeInject: { freePlan: false, paidPlan: true }
+      codeInject: { freePlan: true, paidPlan: true }
    },
    {
       name: 'Unicorn Platform',
@@ -121,7 +121,7 @@ const rawData = [
       designFlexibility: 3,
       seoTools: 2,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: false },
       exportContent: { freePlan: false, paidPlan: true },
@@ -137,7 +137,7 @@ const rawData = [
       designFlexibility: 1,
       seoTools: 2,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: false },
       exportContent: { freePlan: true, paidPlan: true },
@@ -153,7 +153,7 @@ const rawData = [
       designFlexibility: 2,
       seoTools: 4,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: true, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: false },
       exportContent: { freePlan: true, paidPlan: true },
@@ -169,7 +169,7 @@ const rawData = [
       designFlexibility: 5,
       seoTools: 5,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: true, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: false },
       exportContent: { freePlan: false, paidPlan: false },
@@ -185,7 +185,7 @@ const rawData = [
       designFlexibility: 3,
       seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: false, paidPlan: false },
@@ -201,7 +201,7 @@ const rawData = [
       designFlexibility: 3,
       seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: false, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: false, paidPlan: false },
@@ -233,7 +233,7 @@ const rawData = [
       designFlexibility: 2,
       seoTools: 3,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: true, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: true, paidPlan: true },
@@ -249,7 +249,7 @@ const rawData = [
       designFlexibility: 1,
       seoTools: 1,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: true, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: false },
       exportContent: { freePlan: true, paidPlan: true },
@@ -265,7 +265,7 @@ const rawData = [
       designFlexibility: 1,
       seoTools: 2,
       storage: '',
-      limits: null,
+      limits: "",
       customDomain: { freePlan: true, paidPlan: true },
       ecommerce: { freePlan: false, paidPlan: true },
       exportContent: { freePlan: true, paidPlan: true },
@@ -345,62 +345,62 @@ const rawData = [
 const features = {
    easeOfUse: {
       label: "Ease of Use",
-      tooltip: "How easy it is for a beginner to set up and use the platform without technical knowledge. Consider drag-and-drop interfaces, intuitive navigation, and the availability of tutorials/guides.",
+      tooltip: "How easy it is for a beginner to set up and use the platform without technical knowledge.",
       type: "number",
    },
    designFlexibility: {
       label: "Design Flexibility",
-      tooltip: "The level of control you have over the look and feel of your blog. This includes the availability of themes, templates, customization options for layouts, fonts, and colors.",
+      tooltip: "The level of control you have over the look and feel of your blog.",
       type: "number",
    },
    seoTools: {
       label: "SEO Tools",
-      tooltip: "Built-in features that help you optimize your blog posts for search engines. This might include editing titles and meta descriptions, generating sitemaps, and suggesting keywords.",
+      tooltip: "Built-in features that help you optimize your blog posts for search engines.",
       type: "number",
    },
    storage: {
       label: "Storage",
-      tooltip: "The amount of disk space provided to store your blog's content, including text, images, videos, and other files.",
+      tooltip: "The amount of disk space provided to store your blog's media.",
       type: "text",
    },
    limits: {
       label: "Limits",
-      tooltip: "Any restrictions or limitations on the free plan, such as the number of posts, pages, or visitors allowed. Paid plans may offer higher limits or remove these restrictions.",
+      tooltip: "Any restrictions or limitations on the free plan.",
       type: "text",
    },
    customDomain: {
       label: "Custom Domain Support",
-      tooltip: "The ability to use your own unique domain name (e.g., www.yourblogname.com) in the free plan instead of a generic subdomain provided by the blogging platform. This enhances professionalism and brand recognition",
+      tooltip: "The ability to use your own unique domain name.",
       type: "boolean",
    },
    ecommerce: {
       label: "E-commerce",
-      tooltip: "Features that allow you to sell products or services directly from your blog. This includes tools for product listings, shopping carts, payment gateways, and inventory management.",
+      tooltip: "Features that allow you to sell products or services directly from your blog.",
       type: "boolean",
    },
    exportContent: {
       label: "Export Content",
-      tooltip: "The ability to download a backup of your blog's content (posts, pages, images) in a standard format, allowing you to move your blog to another platform if needed.",
+      tooltip: "The ability to download a backup of your blog's content (posts, pages, images).",
       type: "boolean",
    },
    markdown: {
       label: "Markdown Support",
-      tooltip: "Lets you write your blog posts using Markdown, a simple formatting language that's easy to learn and converts cleanly to HTML for web publishing.",
+      tooltip: "The ability that allows you write your blog posts using Markdown.",
       type: "boolean",
    },
    openSource: {
       label: "Open Source",
-      tooltip: "The platform's code is freely available and can be modified or redistributed by developers. This offers greater flexibility and customization potential.",
+      tooltip: "The platform's code is freely available and can be modified or redistributed by developers. ",
       type: "boolean",
    },
    headless: {
       label: "Headless",
-      tooltip: "A headless blogging platform separates the content management system (backend) from the presentation layer (frontend). This allows developers to deliver content to various devices and channels beyond a traditional website.",
+      tooltip: "The ability to use your content via an API to build a custom front-end for your blog.",
       type: "boolean",
    },
    codeInject: {
       label: "Custom Code Injection",
-      tooltip: "The ability to add your own HTML, CSS, and JavaScript code to your blog. This allows for advanced customization of design, functionality, and the integration of third-party tools (e.g., analytics, social widgets, interactive elements).",
+      tooltip: "The ability to add your own HTML, CSS, and JavaScript code to your blog. ",
       type: "boolean",
    },
 }
@@ -422,17 +422,20 @@ const emptyStar = "☆"
 
 const columns = [
    {
-      id: "feature",
-      name: "Feature",
-      formatter: (cell, row) => gridjs.html(`<div class="tooltip">${cell}<span class="tooltiptext">${features[row.cells[1].data].tooltip}</span></div>`)
+      id: "platform",
+      name: gridjs.html(`<div class="tooltip-wrapper"><div>Platforms</div><div class="tooltip"><a href="https://bloggingplatforms.app/blog/best-free-platforms-for-blogging">Free Blogging Platforms</a></div></div>`),
+
+      formatter: (cell, row) => gridjs.html(`<a href="${row.cells[1].data}">${row.cells[0].data}</a>`)
+      // formatter: (cell, row) => gridjs.html(`<div class="tooltip">${cell}<span class="tooltiptext">${features[row.cells[1].data].tooltip}</span></div>`)
    },
    {
-      name: 'featureKey',
+      name: 'platformUrl',
       hidden: true
    }
-].concat(rawData.map((platform) => ({
-   id: platform.name,
-   name: gridjs.html(`<a href="${platform.url}">${platform.name}</a>`),
+].concat(Object.keys(features).map((featureKey) => ({
+   id: featureKey,
+   name: gridjs.html(`<div class="tooltip-wrapper"><div>${features[featureKey].label}</div><div class="tooltip">${features[featureKey].tooltip}</div></div>`),
+
    formatter: (cell, row) => {
       if (Array.isArray(cell)) {
          const freePlan = cell[0];
@@ -445,7 +448,7 @@ const columns = [
       }
 
       // If the cell is a number, it means it's a rating
-      else if (!isNaN(cell)) {
+      else if (typeof cell === "number") {
          return fillStar.repeat(cell) + emptyStar.repeat(5 - cell);
 
       }
@@ -480,17 +483,17 @@ const columns = [
 // ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═  ▶
 // COLUMN CELLS
 // ◀ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ # 
-const data = Object.keys(features).map((featureKey) => {
+const data = rawData.map((platformData) => {
 
-   // Single row for each platform: [Ease of Use, easeOfUse (hidden), ...[featureValues]]
-   const row = [features[featureKey].label, featureKey]
+   // Single row for each platform: [name, url, Ease of Use, easeOfUse (hidden), ...[featureValues]]
+   const row = [platformData.name, platformData.url]
 
    // Iterate through the features and add the corresponding value to the row
-   for (const platform of rawData) {
+   for (const featureKey of Object.keys(features)) {
       if (features[featureKey].type === "number" || features[featureKey].type === "text") {
-         row.push(platform[featureKey]);
+         row.push(platformData[featureKey]);
       } else if (features[featureKey].type === "boolean") {
-         row.push([platform[featureKey].freePlan, platform[featureKey].paidPlan]);
+         row.push([platformData[featureKey].freePlan, platformData[featureKey].paidPlan]);
       }
    }
    return row;
@@ -499,8 +502,23 @@ const data = Object.keys(features).map((featureKey) => {
 new gridjs.Grid({
    columns: columns,
    data: data,
+   style: {
+      th: {
+         padding: "2px",
+         minWidth: "200px",
+         overflow: "visible",
+      },
+      td: {
+         'font-size': "11px",
+         'line-height': "1.2",
+         'font-weight': 300,
+         opacity: 0.8,
+         'border': '1px solid hsl(0, 0%, 80%)',
+
+       }
+   },
    resizable: true,
-   sort: false,
+   sort: true,
    fixedHeader: true,
    height: `${window.innerHeight * 0.96}px`,
 }).render(document.getElementById("wrapper"));
