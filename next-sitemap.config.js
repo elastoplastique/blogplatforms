d8a6bbf5cfc2b31cd3017cdf8c22e373c8b57f12
Elastoplastique <elastoplastique@gmail.com>
2024-04-15 01:10:22 +0300

    fix: robots.txt from sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,1 +1,5 @@
-module.exports={siteUrl:process.env.SITE_URL,generateRobotsTxt:!0,exclude:["/profile","/profile/login","/profile/signup","about"]};
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: false,
+  exclude: ['/profile', '/profile/login', '/profile/signup', 'about'],
+};
