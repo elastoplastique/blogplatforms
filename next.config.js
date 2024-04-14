c8d0c67e1636191db0bc456574f60344a2eb9dfa
Elastoplastique <elastoplastique@gmail.com>
2024-04-15 01:00:15 +0300

    fix: removal of console.logs

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
   compiler: {
     removeConsole:
-      process.env.NODE_ENV === "development" ? true : false,
+      process.env.NODE_ENV === "development" ? false : true,
   },
   experimental: { 
     optimizeCss: true,
